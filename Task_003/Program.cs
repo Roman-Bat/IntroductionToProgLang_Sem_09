840e1a4f26424f41a03d6107b5ff856cf9cd2033
Roman_Batrakov <kyct69@yandex.ru>
2023-10-01 12:14:38 +0300

    Выполнил Task_002

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -4,3 +4,17 @@
 453 -> 12
 45 -> 9
 */
+
+int n = 8;
+
+// 
+
+string ShowNumbers (int n)
+{
+    if(n == m) return $"{m} ";
+    return ShowNumbers(n - 1) + $"{n} ";
+}
+
+
+Console.WriteLine();
+Console.WriteLine($"\nНатуральные числа в промежутке от M до N: \n {ShowNumbers(n)}\n")
