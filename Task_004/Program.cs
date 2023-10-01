4c6533588728481018c1e8670b537288ae36ed25
Roman_Batrakov <kyct69@yandex.ru>
2023-10-01 13:14:29 +0300

    Выполнил Task_004

--- Task_004/Program.cs
+++ Task_004/Program.cs
@@ -4,3 +4,14 @@
 A = 3; B = 5 -> 243 (3⁵)
 A = 2; B = 3 -> 8
 */
+int a = 2;
+int b = 3;
+// Метод который рекурсивно возводит в степень
+int SqurNumbers(int num_1, int num_2)
+{
+   if (num_2 == 0) return 1;
+   return num_1 * SqurNumbers(num_1, num_2 - 1);
+   
+}
+
+Console.WriteLine($"Возводим число {a} в степень {b} = {SqurNumbers(a, b)}");
